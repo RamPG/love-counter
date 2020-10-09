1be425ef979d899ef2d485a6cbf80c179b3393c2
git config --global user.name <vlasovnew@yandex.ru>
2020-10-09 18:25:42 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+import { firebaseConfig } from './config';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const userRef = db.collection('users').doc('token');
 
 export default function App() {
+  const [text, setText] = useState('');
+  userRef.get()
+    .then((res) => {
+      console.log(res.data());
+    });
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{ text }</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
